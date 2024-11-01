fe07c44ddd19acb9506160a7ea1dfbff9510b794
ritsukurumada <ritsu040423@gmail.com>
2024-11-01 19:25:22 +0900

    Add files via upload

---
+++ 20241101.c
@@ -0,0 +1,18 @@
+#include <float.h>
+#include <limits.h>
+#include <stdio.h>
+
+
+int main(void)
+{
+    printf("char型の最大値：%d\n", CHAR_MAX);
+    printf("char型の最小値：%d\n", CHAR_MIN);
+    printf("char型の符号なし最大値:%d\n", UCHAR_MAX);
+    
+    printf("short型の最大値:%d\n", SHRT_MAX);
+    printf("short型の最小値:%d\n", SHRT_MIN);
+    
+    
+    
+    return 0;
+}
