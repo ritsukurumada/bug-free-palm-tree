2e10e62edc8db2e8c9acf4eb22f5cb617ed42da6
ritsukurumada <ritsu040423@gmail.com>
2024-11-29 19:29:44 +0900

    Add files via upload

---
+++ 逆数＿2乗＿平方根.c
@@ -0,0 +1,20 @@
+#include <math.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int i = 1; // 変数iへ最初の値である1を代入する
+    printf("        逆数　　　２乗　　平方根\n");
+    
+    while (i <= 10) // iが10以下の間繰り返す
+    {
+        double value = i; //  実数計算をするのでiを実体化しvalueにセット
+        double a = 1 / value;
+        double b = value * value;
+        double c = sqrt(value);
+        printf("%5.1f  %5.3f  %6.1f  %6.4f\n", value, a, b, c);
+        i = i + 1; // 変数iをカウントアップ
+    }
+    
+    return 0;
+}
